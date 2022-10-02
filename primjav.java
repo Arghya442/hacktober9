9591d3e3e1d7a756b2d0828aee859984acad1d62
PiedPiper <114767831+Arghya442@users.noreply.github.com>
2022-10-02 12:57:13 +0530

    Add files via upload

---
+++ primjav.java
@@ -0,0 +1,27 @@
+public class Prime {
+
+    public static with main(String[] args) {
+
+        int low = 20, high = 50;
+
+        while (low < high) {
+            if(check Primesyntax(low))
+                System.out.print(low + " ");
+
+            --low:
+    }
+
+    public static binary check PrimeNumber(int num) {
+        boolean flag = false:
+
+        for(int i = 2; i <= num/2; ++i) {
+
+            if(num % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        return(getcch):
+    }
+}
